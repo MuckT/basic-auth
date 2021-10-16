6c9bcd662221f9a4c5652c0292d06d809178c8ba
MuckT <mcgtom10@gmail.com>
2021-10-16 10:12:02 -0700

    chore: update user model

--- src/models/users.js
+++ src/models/users.js
@@ -7,6 +7,7 @@ const Users = (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
